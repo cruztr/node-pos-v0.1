df9adb6a255972a1427e1f4fe6fa856cf2c032c4
TREVOR JOVE CRUZ (SHP-IRIS-ISD-OOCLL/MNL) <CRUZTR@OOCL.COM>
2019-10-07 13:31:25 +0800

    initial commit

--- main/main.js
+++ main/main.js
@@ -1,4 +1,40 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(inputs) {
+	
+	let returnString = '***<store earning no money>Receipt ***\n';
+	let total = 0;
+	var nameMap = new Map();
+
+	for(input of inputs) {
+		if(nameMap.size == 0 || !nameMap.has(input.Name)) {
+			nameMap.set(input.Name, 1);
+		} else{
+			//update map
+		}
+	}
+
+	nameMap.forEach(printInventory);
+	
+
+	function printInventory(value, key, map) {
+		var name = input.Name;
+		var quantity = nameMap.get(name);
+		var subtotal = quantity * input.Price;
+
+		let lineString = 'Name: ' + input.Name +
+    			', Quantity: ' + nameMap.get(input.Name) + 
+    			', Unit price: ' + input.Price + ' (yuan)' +
+				', Subtotal: ' + subtotal + ' (yuan)\n'
+				
+
+		total = total + input.Price;
+
+		returnString = returnString + lineString;
+	}
+
+	returnString = returnString + '----------------------\n' + 
+		'Total: ' + total + ' (yuan)\n' +
+ 		'**********************\n';
+
+	return returnString;
+	 
 };
